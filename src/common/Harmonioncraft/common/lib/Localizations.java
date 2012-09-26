f603a1772e267452670262b71152856a9e910bb3
alexbegt <traincrazyb@aol.com>
2012-09-26 19:39:21 -0400

    Added lang

---
+++ src/common/Harmonioncraft/common/lib/Localizations.java
@@ -0,0 +1,21 @@
+package Harmonioncraft.common.lib;
+
+/**
+ * Localizations
+ * 
+ * Library containing all mod language related files
+ * 
+ * @author Alexbegt,DJP
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+public class Localizations {
+
+	private static final String LANG_RESOURCE_LOCATION = "/Harmonioncraft/client/lang/";
+
+	public static String[] localeFiles = {
+		LANG_RESOURCE_LOCATION + "en_US.xml",
+		LANG_RESOURCE_LOCATION + "nl_NL.xml"
+	};
+
+}
