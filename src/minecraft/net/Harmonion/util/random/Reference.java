041bba95daf329fb2ff8a384cbf243efc753fbf8
Alexander Behrhof <traincrazyb@aol.com>
2013-03-19 15:43:12 -0400

    Cleaning Reference

--- src/minecraft/net/Harmonion/util/random/Reference.java
+++ src/minecraft/net/Harmonion/util/random/Reference.java
@@ -10,9 +10,6 @@ package net.Harmonion.util.random;
  * 
  */
 public class Reference {
-	
-	/* Debug Mode On-Off */
-    public static final boolean DEBUG_MODE = false;
 
     /* General Mod related constants */
     public static final String MOD_ID = "HMC";
@@ -20,26 +17,8 @@ public class Reference {
     public static final String VERSION = "0.0.0.7";
     public static final String CHANNEL_NAME = MOD_ID;
     public static final String LOGGER_PREFIX = "[" + MOD_ID + "] ";
-    public static final int SECOND_IN_TICKS = 20;
-    public static final int SHIFTED_ID_RANGE_CORRECTION = 256;
     public static final String SERVER_PROXY_CLASS = "net.Harmonion.util.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "net.Harmonion.client.ClientProxy";
-    public static final String SERVER_NETWORK_PROXY_CLASS = "net.Harmonion.network.ProxyNetwork";
-    public static final String CLIENT_NETWORK_PROXY_CLASS = "net.Harmonion.client.network.ClientProxyNetwork";
-    
-    /* Configuration related constants */
-    public static final String ENABLE_VERSION_NOTICE = "enable_version_notice";
-    public static final String ENABLE_SOUNDS = "enable_sounds";
-    public static final String ENABLE_PARTICLE_FX = "enable_particle_fx";
-    public static final String AUTO_RESOLVE_BLOCK_IDS = "auto_resolve_block_ids";
-    
-    /* Gui related constants */
-    public static final String GUI_CALCINATOR_NAME = "gui.calcinator.name";
-    
-    /* General Tile Entity related constants */
-    public static final String TE_GEN_OWNER_NBT_TAG_LABEL = "owner";
-    public static final String TE_GEN_STATE_NBT_TAG_LABEL = "state";
-    public static final String TE_GEN_DIRECTION_NBT_TAG_LABEL = "direction";
     
     /* Texture related constants */
     public static final String SPRITE_SHEET_LOCATION = "/net/Harmonion/client/textures/";
